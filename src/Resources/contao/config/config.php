d9fc6ad5a94699e34eece2a96367840a0f0a3d1a
fritzmg <fmg@inspiredminds.at>
2022-01-03 11:27:29 +0000

    use class_exists to check for bundles

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -12,19 +12,16 @@ declare(strict_types=1);
 
 use Contao\CalendarBundle\ContaoCalendarBundle;
 use Contao\NewsBundle\ContaoNewsBundle;
-use Contao\System;
 use InspiredMinds\ContaoSiblingNavigation\Module\SiblingNavigationEvents;
 use InspiredMinds\ContaoSiblingNavigation\Module\SiblingNavigationNews;
-
-$bundles = System::getContainer()->getParameter('kernel.bundles');
 
 /*
  * Front end modules
  */
-if (\in_array(ContaoNewsBundle::class, $bundles, true)) {
+if (class_exists(ContaoNewsBundle::class)) {
     $GLOBALS['FE_MOD']['news']['sibling_navigation_news'] = SiblingNavigationNews::class;
 }
 
-if (\in_array(ContaoCalendarBundle::class, $bundles, true)) {
+if (class_exists(ContaoCalendarBundle::class)) {
     $GLOBALS['FE_MOD']['events']['sibling_navigation_events'] = SiblingNavigationEvents::class;
 }
