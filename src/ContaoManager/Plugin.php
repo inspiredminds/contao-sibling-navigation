49e75d4e712842d5b5bcae814698692a96145c7c
fritzmg <fmg@inspiredminds.at>
2022-01-03 17:01:40 +0000

    fix bundle loading order

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -17,6 +17,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\NewsBundle\ContaoNewsBundle;
 use InspiredMinds\ContaoSiblingNavigation\ContaoSiblingNavigationBundle;
 
 /**
@@ -31,7 +32,12 @@ class Plugin implements BundlePluginInterface
     {
         return [
             BundleConfig::create(ContaoSiblingNavigationBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class, 'news_sorting', ContaoCalendarBundle::class]),
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                    ContaoCalendarBundle::class,
+                    ContaoNewsBundle::class,
+                    'news_sorting'
+                ]),
         ];
     }
 }
